55b59b8813ad009b637a78203befedf0ef9ecd39
oneoneoneuser <161770825+oneoneoneuser@users.noreply.github.com>
2024-03-01 19:19:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,23 +14,21 @@ let PORT = process.env.PORT || 8080
 //         optionSuccessStatus: 200
 //     }
 // ))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
-app.use(cors())
 
 app.use('/api/user', require('./routers/UserRoutes'))
 app.use('/api/player', require('./routers/PlayerRoutes'))
-
-connection()
-
-// app.get('/', (req, res) => {
-//     return res.json({ success: true, msg: 'Hello!'})
-// })
 
 app.use(express.static(path.join(__dirname, './client/build')))
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 })
 
-app.listen(PORT, () => console.log(`SERVER IS RUN ON PORT: http://localhost:${PORT}`))
+//Connect to the database before listening
+connection().then(() => 
+    app.listen(PORT, () => console.log(`SERVER IS RUN ON PORT: http://localhost:${PORT}`))
+})
+
